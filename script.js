37527c27c6ad904155ff8e0a525ceb92db2083c4
Bagus550 <80018538+Bagus550@users.noreply.github.com>
2024-10-25 08:54:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDetails(project) {
+    const details = project.querySelector('.details');
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+}
